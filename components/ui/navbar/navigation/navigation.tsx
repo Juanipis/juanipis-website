e5280d41c02a51cb44917f2b81611e8a0a451819
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-02-22 15:23:05 -0500

    updated navigation

--- components/ui/navbar/navigation/navigation.tsx
+++ components/ui/navbar/navigation/navigation.tsx
@@ -2,21 +2,31 @@ import { Button, Flex } from "@radix-ui/themes";
 
 import { ReactNode } from "react";
 import { ThemeToggle } from "../theme-mode/ThemeToggle";
+import Link from "next/link";
 
-const NavigationButton = ({ children }: { children: ReactNode }) => {
+interface NavigationButtonProps {
+  children: ReactNode;
+  href: string;
+}
+const NavigationButton: React.FC<NavigationButtonProps> = ({
+  children,
+  href,
+}) => {
   return (
-    <Button mr="1" ml="1" variant="surface">
-      {children}
-    </Button>
+    <Link href={href}>
+      <Button mr="1" ml="1" variant="surface">
+        {children}
+      </Button>
+    </Link>
   );
 };
 
 const Navigation = () => {
   return (
     <Flex align="center">
-      <NavigationButton>Home</NavigationButton>
-      <NavigationButton>Projects</NavigationButton>
-      <NavigationButton>Contact</NavigationButton>
+      <NavigationButton href="/">Home</NavigationButton>
+      <NavigationButton href="/projects">Projects</NavigationButton>
+      <NavigationButton href="/contact">Contact</NavigationButton>
       <ThemeToggle />
     </Flex>
   );
