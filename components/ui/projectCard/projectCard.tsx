4a84671d21cbb9927a10952ce3983adeae4bf46c
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-02-23 14:25:26 -0500

    wip projects

---
+++ components/ui/projectCard/projectCard.tsx
@@ -0,0 +1,80 @@
+import { Card, Flex, Inset, Strong, Text } from "@radix-ui/themes";
+import { useCssLibPreference } from "../../CssLibPreference";
+import Image from "next/image";
+
+interface ProjectCardProps {
+  title: string;
+  description: string;
+  image: string;
+  technologies: React.FC<SvgProps>[];
+  url: string;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  title,
+  description,
+  image,
+  technologies,
+  url,
+}) => {
+  const { accentColor } = useCssLibPreference();
+
+  return (
+    <Card size="2" style={{ maxWidth: 320, maxHeight: 350 }}>
+      <Inset clip="padding-box" side="top" pb="current">
+        <Image
+          src={image}
+          alt={title}
+          width="320"
+          height="180"
+          style={{
+            display: "block",
+            objectFit: "cover",
+            width: "100%",
+            height: 140,
+            backgroundColor: "var(--gray-5)",
+          }}
+        />
+      </Inset>
+      <Flex direction="column" justify="between" style={{ height: 150 }}>
+        <Flex direction="column">
+          <Text size="3" weight="medium" mt="3">
+            {title}
+          </Text>
+          <Text size="2" mt="2" mr="3">
+            {description}
+          </Text>
+        </Flex>
+
+        <div>
+          <Flex wrap="wrap" gap="2" justify="center" mt="3" mb="1">
+            {technologies.map((Logo, index) => (
+              <Logo
+                key={index}
+                height="20"
+                width="20"
+                fill={accentColor === "crimson" ? "#5C1839" : "#004074"}
+              />
+            ))}
+          </Flex>
+          <Text size="2" ml="3">
+            <Strong>
+              <a
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{
+                  color: accentColor === "crimson" ? "#5C1839" : "#004074",
+                }}
+              >
+                View project
+              </a>
+            </Strong>
+          </Text>
+        </div>
+      </Flex>
+    </Card>
+  );
+};
+
+export default ProjectCard;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,9 +1,31 @@
+import { Flex } from "@radix-ui/themes";
+import PostgreSQLLogo from "../../components/ui/logos/postgresqlLogo";
+import PythonLogo from "../../components/ui/logos/pythonLogo";
+import ProjectCard from "../../components/ui/projectCard/projectCard";
+import MongoDBLogo from "../../components/ui/logos/mongodbLogo";
+import DockerLogo from "../../components/ui/logos/dockerLogo";
+import HTMLLogo from "../../components/ui/logos/htmlLogo";
+import JavascriptLogo from "../../components/ui/logos/javascriptLogo";
+import FirebaseLogo from "../../components/ui/logos/firebaseLogo";
+
 function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
-      <p>Here are some of my projects</p>
-    </div>
+    <Flex ml="3" mr="2" mt="5" gap="7">
+      <ProjectCard
+        title={"Backend para una tienda online 🛒"}
+        description={"Backend necesario para una tienda online."}
+        image="/banner_images/backend_tienda_online.webp"
+        technologies={[PythonLogo, PostgreSQLLogo, MongoDBLogo, DockerLogo]}
+        url={"https://github.com/Juanipis/backend_tienda_online"}
+      />
+      <ProjectCard
+        title="AprendIoT 🌐"
+        description="Web educativa sobre IoT. Fue un proyecto de ingeniería 1 en la EIA."
+        image="/banner_images/aprendiot.webp"
+        technologies={[HTMLLogo, JavascriptLogo, FirebaseLogo]}
+        url={"https://github.com/Juanipis/AprendIoT"}
+      />
+    </Flex>
   );
 }
 
